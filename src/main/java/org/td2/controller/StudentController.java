54c26bfe848e0e20eef1e6c2d570600699a9016d
colombefioren <colomberakotonjanahary@gmail.com>
2026-03-17 16:49:38 +0300

    feat: import service in controller

--- src/main/java/org/td2/controller/StudentController.java
+++ src/main/java/org/td2/controller/StudentController.java
@@ -4,11 +4,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.td2.entity.Student;
+import org.td2.service.StudentService;
 
 import java.util.List;
 
 @RestController
 public class StudentController {
+
+    private final StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
 
      @PostMapping("/students")
     public List<Student> createStudents(@RequestBody List<Student> students){
