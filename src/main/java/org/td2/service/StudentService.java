b5e771ae3fc341e74ad52253f57aee8bef1fdd71
colombefioren <colomberakotonjanahary@gmail.com>
2026-03-17 16:48:44 +0300

    feat: import repository in service

--- src/main/java/org/td2/service/StudentService.java
+++ src/main/java/org/td2/service/StudentService.java
@@ -1,12 +1,19 @@
 package org.td2.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.td2.entity.Student;
+import org.td2.repository.StudentRepository;
 
 import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository repository;
+    public StudentService(StudentRepository repository) {
+        this.repository = repository;
+    }
 
     public List<Student> createStudents(List<Student> students){
         return repository.createStudents(students);
