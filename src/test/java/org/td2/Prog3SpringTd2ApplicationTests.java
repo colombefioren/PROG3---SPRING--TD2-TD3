37210afdbbdbe5b7ae2c153466f1fcb65dbcfd1c
colombefioren <colomberakotonjanahary@gmail.com>
2026-03-17 16:33:11 +0300

    chore: setup spring project

---
+++ src/test/java/org/td2/Prog3SpringTd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.restservice.prog3springtd2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3SpringTd2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
