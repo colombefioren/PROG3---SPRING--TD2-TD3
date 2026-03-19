a17d402cc59d95586239189b3809250400fdcbdc
colombefioren <colomberakotonjanahary@gmail.com>
2026-03-19 07:31:28 +0300

    refactor: add initial data inside in memory student list

--- src/main/java/org/td2/entity/Student.java
+++ src/main/java/org/td2/entity/Student.java
@@ -1,8 +1,14 @@
 package org.td2.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
     private String reference;
     private String firstName;
--- src/main/java/org/td2/repository/StudentRepository.java
+++ src/main/java/org/td2/repository/StudentRepository.java
@@ -4,13 +4,20 @@ import org.springframework.stereotype.Repository;
 import org.td2.entity.Student;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Repository
 public class StudentRepository {
 
 
-    List<Student> list = new ArrayList<>();
+    List<Student> list = new ArrayList<>(Collections.singletonList(Student.builder()
+            .reference("1")
+            .firstName("Coco")
+            .lastName("Fioren")
+            .age(18)
+            .build()));
 
     public List<Student> createStudents(List<Student> students) {
         list.addAll(students);
