e3cafae297280f461c14970a4ee662d0882f603c
colombefioren <colomberakotonjanahary@gmail.com>
2026-03-19 08:06:33 +0300

    feat: use response entity in get welcome controller

--- src/main/java/org/td2/controller/WelcomeController.java
+++ src/main/java/org/td2/controller/WelcomeController.java
@@ -1,5 +1,7 @@
 package org.td2.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
     @GetMapping("/welcome")
-    public String welcome(@RequestParam String name) {
-        return "Welcome " + name;
+    public ResponseEntity<String> welcome(@RequestParam String name) {
+        if (name == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Welcome " + name);
     }
 }
