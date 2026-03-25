147643412a815b820dbda416359fde37bc51639e
colombefioren <colomberakotonjanahary@gmail.com>
2026-03-25 17:10:38 +0300

    feat: catch bad request exceptoin in create students controller

--- src/main/java/org/td2/controller/StudentController.java
+++ src/main/java/org/td2/controller/StudentController.java
@@ -1,21 +1,20 @@
 package org.td2.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.td2.entity.Student;
+import org.td2.exception.BadRequestException;
 import org.td2.service.StudentService;
 
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 public class StudentController {
 
     private final StudentService service;
-
-    public StudentController(StudentService service) {
-        this.service = service;
-    }
 
     @PostMapping("/students")
     public ResponseEntity<List<Student>> createStudents(@RequestBody List<Student> students) {
@@ -24,6 +23,8 @@ public class StudentController {
             return ResponseEntity
                     .status(HttpStatus.CREATED)
                     .body(service.createStudents(students));
+        } catch (BadRequestException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         } catch (RuntimeException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
